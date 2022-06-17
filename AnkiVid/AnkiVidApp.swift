477acbb096b474fb33123164a8612fb624e0044b
kibrom <dstkibrom@gmail.com>
2022-06-17 13:37:58 +0900

    Initial Commit

---
+++ AnkiVid/AnkiVidApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnkiVidApp.swift
+//  AnkiVid
+//
+//  Created by Kibrom Desta Araya on 2022/06/17.
+//
+
+import SwiftUI
+
+@main
+struct AnkiVidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
